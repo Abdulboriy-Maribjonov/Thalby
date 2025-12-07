8e554367704c1e3d32eb7471c68b1cd4f02ff840
Abdulboriy-Maribjonov <abdulboriymaribjonov08@gmail.com>
2025-12-07 15:44:23 +0500

    Thalby navbar done

---
+++ script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("menu")
+const closeMenu = document.getElementById("closeMenu")
+const list = document.getElementById("list")
+const appMenu = document.getElementById("app-menu")
+const appList = document.getElementById("app-list")
+
+menu.addEventListener("click",()=>{
+    list.classList.add("menu-active")
+})
+closeMenu.addEventListener("click",()=>{
+    list.classList.remove("menu-active")
+})
+appMenu.addEventListener("click",()=>{
+    appList.classList.toggle("app-active")
+})
